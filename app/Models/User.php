1ed011d2e68394a88ae6c0b5f4235d960b294592
ferit erçıktı <feritercikti@gmail.com>
2023-09-11 04:05:17 +0300

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function events(): HasMany {
+     
+       return $this->hasMany(Event::class);
+    }
+
+    public function galleries(): HasMany {
+     
+       return $this->hasMany(Gallery::class);
+    }
+
+    public function comments(): HasMany {
+     
+       return $this->hasMany(Comment::class);
+    }
+
+    public function likes(): HasMany {
+     
+       return $this->hasMany(Like::class);
+    }
+
+    public function attendings(): HasMany {
+     
+       return $this->hasMany(Attending::class);
+    }
+
+}
